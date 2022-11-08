9c82347d5f19d86b5478279bb4a927cda3ea4074
Adhil Akbar <356adhil@gmail.com>
2022-11-08 16:25:02 +0530

    Admin Add User.

--- controller/adminController/adminController.js
+++ controller/adminController/adminController.js
@@ -1,3 +1,4 @@
+const User = require("../../model/signUpModel")
 
 
 const admin = {email: 'admin@gmail.com', password: '1100'}
@@ -6,7 +7,7 @@ module.exports = {
 
     loginPage:(req,res)=>{
         if(req.session.admin){
-            res.redirect('/home')
+            res.redirect('/admin/home')
         }else{
             res.render('admin/adminLogin')
         }
@@ -20,6 +21,49 @@ module.exports = {
             res.redirect('/admin')
         }
     },
+    getHome:async(req,res)=>{
+        if(req.session.admin){
+        const usersData =  await  User.find({is_admin:0})
+            res.render('admin/table',{users:usersData})
+        }else{
+            res.redirect('/admin')
+        }
+
+    },
+    addUser:(req,res)=>{
+        try {
+            res.render('admin/newUser')
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+    postaddUser:async(req,res)=>{
+        try {
+            const firstname = req.body.firstname
+            const lastname = req.body.lastname
+            const email = req.body.email
+            const password = req.body.password
+
+            const user = new User({
+                first_name:firstname,
+                last_name:lastname,
+                email:email,
+                password:password
     
+            })
+            const userData = await user.save()
+            if(userData){
+                res.redirect('/admin')
+            }else{
+                res.render('admin/newUser')
+            }
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    // add new user
+
+
 
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,9 @@ const adminController = require('../controller/adminController/adminController')
 
 router.get('/',adminController.loginPage)
 router.post('/home',adminController.postHome)
+router.get('/home',adminController.getHome)
 
-
-
+router.get('/add',adminController.addUser)
+router.post('/add',adminController.postaddUser)
 
 module.exports = router
